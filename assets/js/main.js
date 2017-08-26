0fed8bfefa97623e451609bf601dd6c82d83746d
Jesse Dillman <jesse@jdillman.com>
2017-08-26 17:28:15 -0400

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
-	
+
+	var searchIcon = $('#searchIcon');
+	var searchForm = $('#searchForm');
+	var searchOpenState = false;
+
+	searchIcon.click(function(e) {
+		e.preventDefault();
+		if (searchOpenState) {
+			searchForm.css({"transform":"translate(0px, 0px)"});
+			searchIcon.removeClass('active');
+			searchOpenState = false;
+		} 
+		else{
+			searchForm.css({"transform":"translate(" + -1 * (searchForm.outerWidth()) + "px, 0px)"});
+			searchIcon.addClass('active');
+			searchOpenState = true;
+		};
+	});
+
+	var mobileOpenState = false;
+	var mobileNav = $('#mobileNav');
+	var sideMenu = $('#sideMenu');
+
+	mobileNav.click(function() {
+		if (mobileOpenState) {
+			sideMenu.css({"transform":"translate(0px, 0px)"});
+			mobileNav.removeClass('open');
+			mobileOpenState = false;
+		} 
+		else{
+			sidebarContent('#mainNav');
+			sideMenu.css({"transform":"translate(" + (sideMenu.outerWidth()) + "px, 0px)"});
+			mobileNav.addClass('open');
+			mobileOpenState = true;
+		};
+	});
+
+	function sidebarContent(element) {
+		sideMenu.html($(element).clone()).html();
+	}
 });
--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,63 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	sass = require('gulp-sass'),
+	cmq = require('gulp-combine-mq'),
+	autoprefixer = require('gulp-autoprefixer'),
 	imagemin = require('gulp-imagemin'),
 	twig = require('gulp-twig');
 
-gulp.task('default', function () {
-	gulp.src('./assets/js/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('./public/assets/js'));
-
-	gulp.src('./assets/js/vendor/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('./public/assets/js/vendor'));
-
-	gulp.src('./assets/sass/**/*.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./public/assets/css'));
-
-	gulp.src('./assets/img/*')
-	.pipe(imagemin())
-	.pipe(gulp.dest('./public/assets/img'));
-
-	gulp.src('./*.html')
-	.pipe(twig())
-	.pipe(gulp.dest('./public'));
+gulp.task('js', function() {
+	return gulp.src('./assets/js/*.js')
+		.pipe(uglify())
+		.on('error', handleError)
+		.pipe(gulp.dest('./public/assets/js'));
 });
+
+gulp.task('vendor-js', function() {
+	return gulp.src('./assets/js/vendor/*.js')
+		.pipe(uglify())
+		.on('error', handleError)
+		.pipe(gulp.dest('./public/assets/js/vendor'));
+});
+
+gulp.task('sass', function() {
+	return gulp.src('./assets/sass/**/*.scss')
+		.pipe(sass())
+		.on('error', handleError)
+		.pipe(cmq())
+		.pipe(autoprefixer({cascade: false}))
+		.pipe(gulp.dest('./public/assets/css'));
+});
+
+gulp.task('img', function() {
+	return gulp.src('./assets/img/*')
+		.pipe(imagemin())
+		.on('error', handleError)
+		.pipe(gulp.dest('./public/assets/img'));
+});
+
+gulp.task('html', function() {
+	return gulp.src('./*.html')
+		.pipe(twig())
+		.on('error', handleError)
+		.pipe(gulp.dest('./public'));
+});
+
+gulp.task('start', function() {
+	gulp.start('js', 'vendor-js', 'sass', 'img', 'html');
+});
+
+gulp.task('watch', ['start'], function() {
+	gulp.watch('./assets/js/*.js', ['js']);
+	gulp.watch('./assets/js/vendor/*.js', ['vendor-js']);
+	gulp.watch('./assets/sass/**/*.scss', ['sass']);
+	gulp.watch('./assets/img/*', ['img']);
+	gulp.watch('./*.html', ['html']);
+});
+
+gulp.task('default', ['start']);
+
+function handleError(err) {
+	console.log(err.toString());
+	this.emit('end');
+}
